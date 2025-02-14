cc5786b9cf3a77cb31e423d87c096092ebf6c026
Shelby <aryanmaeroxas@gmail.com>
2025-02-14 14:55:38 +0800

    add upload feature for google drive lite

--- app/protected/google-drive-lite/page.tsx
+++ app/protected/google-drive-lite/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ImageTable from "./ImageTable";
+import { createClient } from "@/utils/supabase/client";
 
 interface Props {
   searchParams: Promise<{ sortOrder: string }>;
@@ -9,12 +10,41 @@ interface Props {
 
 export default function GoogleDriveLite(props: Props) {
   const [searchQuery, setSearchQuery] = useState("");
-
   const [sortOrder, setSortOrder] = useState<string>("");
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
 
-  props.searchParams.then((params) =>
-    setSortOrder(params.sortOrder || "upload")
-  );
+  const supabase = createClient();
+
+  useEffect(() => {
+    props.searchParams.then((params) => {
+      setSortOrder(params.sortOrder);
+    });
+  }, [props.searchParams]);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files?.[0]) {
+      setSelectedFile(event.target.files[0]);
+    }
+  };
+
+  // Upload file using standard upload
+  async function uploadFile(file: File) {
+    const timestamp = Date.now();
+    const filePath = `images/${timestamp}-${file.name}`;
+    const { data, error } = await supabase.storage
+      .from("google-drive-lite")
+      .upload(filePath, file, { contentType: file.type });
+    if (error) {
+      console.log("Upload failed:", error.message);
+      return null;
+    } else {
+      console.log("File uploaded successfully:", data);
+    }
+    const fileName = data.path.split("/").pop();
+    console.log("Uploaded File Name:", fileName);
+
+    return fileName;
+  }
 
   return (
     <div className="p-4 mx-auto">
@@ -39,10 +69,24 @@ export default function GoogleDriveLite(props: Props) {
           type="file"
           className="file-input file-input-bordered w-full max-w-xs"
           accept="image/*"
+          onChange={handleFileChange}
         />
+        <button
+          type="button"
+          title="Upload"
+          className="btn btn-primary ml-3"
+          onClick={() => {
+            if (!selectedFile) {
+              alert("Please select a file before uploading.");
+              return;
+            }
+            uploadFile(selectedFile);
+          }}
+        >
+          UPLOAD
+        </button>
       </div>
 
-      {/* 🔹 Pass searchQuery to ImageTable */}
       <ImageTable sortOrder={sortOrder} searchQuery={searchQuery} />
     </div>
   );
